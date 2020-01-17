4f5addadd6ddac1bbc5feed777ffc4b67e0b08c7
SN <sacha.nocetto@hotmail.com>
2020-01-17 02:53:25 -0300

    adding initial configuration

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='App-container'>
+        <div className='App-content'>
+          <h1>Hello, world!</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
