c6ccf23bf479138f20167ef0205413db59f25d45
SN <sacha.nocetto@hotmail.com>
2020-01-17 23:53:39 -0300

    first structure

---
+++ src/containers/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+//Components
+import Display from './Display';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='App-container'>
+        <div className='App-content'>
+          <Display />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Display.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Display = () => {
+  return (
+    <div className="Display">
+      <div className="Display-container">
+        <div className="Display-item">
+          CANVAS
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Display;
