9153d7f20d8704c476aee4aed6a1894286c68e0e
SN <sacha.nocetto@hotmail.com>
2020-02-14 06:13:08 -0300

    delete comment

--- webpack.config.js
+++ webpack.config.js
@@ -29,13 +29,6 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      // {
-      //   test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-      //   loader: 'url-loader',
-      //   options: {
-      //     limit: 8192,
-      //   },
-      // },
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
