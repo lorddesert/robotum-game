4c37b497bae4148b528d4134f6127a32a36d9172
SN <sacha.nocetto@hotmail.com>
2020-02-14 04:55:36 -0300

    hotfix import

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ import './styles/reset.css';
 import './styles/App.css';
 
 // Images
-import './styles/images/background.jpg';
-import './styles/images/cara.png';
-import './styles/images/gnaro.png';
-import './styles/images/cara.png';
-import './styles/images/life-bar.png';
-import './styles/images/life-count.png';
-import './styles/images/lifepoint-1.png';
-import './styles/images/pointer-1.png';
-import './styles/images/gamepad-1.jpg'
+// import './styles/images/background.jpg';
+// import './styles/images/cara.png';
+// import './styles/images/gnaro.png';
+// import './styles/images/cara.png';
+// import './styles/images/life-bar.png';
+// import './styles/images/life-count.png';
+// import './styles/images/lifepoint-1.png';
+// import './styles/images/pointer-1.png';
+// import './styles/images/gamepad-1.jpg'
 
 ReactDOM.render(<App />, document.getElementById('root'));
