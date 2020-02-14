dc08063c4135b8fb25e3e674ac68af21d3989f36
SN <sacha.nocetto@hotmail.com>
2020-02-14 05:18:25 -0300

    hotfix

--- webpack.config.js
+++ webpack.config.js
@@ -29,21 +29,21 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-        },
-      },
       // {
-      //   test: /\.(png|jpe?g|gif)$/i,
-      //   use: [
-      //     {
-      //     loader: 'file-loader',
-      //     },
-      //   ],
+      //   test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+      //   loader: 'url-loader',
+      //   options: {
+      //     limit: 8192,
+      //   },
       // },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+          loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [
