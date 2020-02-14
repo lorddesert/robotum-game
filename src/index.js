9ee29d357a3433d215f7cb09263331a9acd92cff
SN <sacha.nocetto@hotmail.com>
2020-02-14 03:56:23 -0300

    update import css

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 
+// Css
+import './styles/Display.css';
+import './styles/Header.css';
+import './styles/reset.css';
+import './styles/App.css';
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
