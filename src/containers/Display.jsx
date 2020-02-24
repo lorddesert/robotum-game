3190ff2da256cf09e2bba018c0561e2b1f24648b
SN <sacha.nocetto@hotmail.com>
2020-02-24 07:53:32 -0300

    update containers

--- src/containers/Display.jsx
+++ src/containers/Display.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Game from '../components/Game';
+import TransitionAnm from '../components/TransitionAnm';
 
 const Display = () => {
   return (
     <div className="Display">
       <div className="Display-container">
+        <TransitionAnm />
         <Game />
       </div>
     </div>
