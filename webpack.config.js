c30dce4ad11c6d09ba64b9317a99b4aca8d7d40b
SN <sacha.nocetto@hotmail.com>
2020-02-28 05:32:01 -0300

    file loader now support mp3/4 files

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|mp3?4)$/i,
         use: [
           {
           loader: 'file-loader',
