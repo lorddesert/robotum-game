539290f882e2d8086212a8ca6ea461d1670f04c6
SN <sacha.nocetto@hotmail.com>
2020-03-08 07:16:40 -0300

    update loader

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif|mp3|mp4)$/i,
+        test: /\.(png|jpe?g|gif|mp3|mp4|ogg)$/i,
         use: [
           {
           loader: 'file-loader',
